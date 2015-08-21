edac8f71c541a31a0748cfffc66039d5f776ba77
abelHan <talance@126.com>
2015-08-21 14:54:24 +0800

    abel
    richText versition1.0

--- src/app/Component/MMRichText.lua
+++ src/app/Component/MMRichText.lua
@@ -1,22 +1,90 @@
 --[[
 	富文本框
 	MMRichText
+	1、多样化的文本显示
+	2. 显示图片和一些特殊元素
+	3. 支持图片，文字的超链接功能
+	4. 换行
+	5. 支持设置控件爱的最大显示行数
+	6. 支持数据的分类显示
+	
+	注释：待优化
+	
 --]]
 
 
 
 local MMRichText = class("MMRichText",function ()
-
-
+	return ccui.RichText:create()
 end)
 
 
-function MMRichText:ctor()
-
-
+function MMRichText:ctor(areaSize)
+	self:ignoreContentAdaptWithSize(false)
+	self:setContentSize(areaSize)
+	self.totalElement = 0
 end
 
 
+--[[
+	设置元素key,config
+	tag: number 标号
+	color: color3d_tabel   cc.c3b(255,0,255)
+	opacity: number	
+	text: string
+	fontName: string
+	fontSize: fontSize
+	
+	filePath: string
+	
+	customNode: cc.Node
+--]]
+function MMRichText:addText(config)
+	if config == nil then return end
+	
+	self.totalElement = self.totalElement + 1
+	local tag = config.tag or self.totalElement
+	local color = config.color or cc.c3b(255,255,255)
+	local opacity = config.opacity or 255
+	local text = config.text or " "
+	local fontName = config.fontName or "Helvetica"
+	local fontSize = config.fontSize or 22
+	
+	local ele = ccui.RichElementText:create(tag,color,opacity,text,fontName,fontSize)
+	self:pushBackElement(ele)
+	return ele
+end
+
+function MMRichText:addImage(config)
+	if config == nil or config.filePath == nil then return end
+	
+	self.totalElement = self.totalElement + 1
+	local tag = config.tag or self.totalElement
+	local color = config.color or cc.c3b(255,255,255)
+	local opacity = config.opacity or 255
+	local filePath = config.filePath
+	
+	local ele = ccui.RichElementImage:create(tag,color,opacity,filePath)
+	self:pushBackElement(ele)
+	return ele
+end
+
+function MMRichText:addCustomNode(config)
+	if config == nil or config.customNode == nil then return end
+	
+	self.totalElement = self.totalElement + 1
+	local tag = config.tag or self.totalElement
+	local color = config.color or cc.c3b(255,255,255)
+	local opacity = config.opacity or 255
+	local customNode = config.customNode
+	
+	local ele = ccui.RichElementCustomNode:create(tag,color,opacity,customNode)
+	self:pushBackElement(ele)
+	return ele
+end
 
 
 return MMRichText
+
+
+
--- src/app/view/LoginScene.lua
+++ src/app/view/LoginScene.lua
@@ -5,6 +5,7 @@
 
 
 local MMListView = requireM(g_require_com .. "MMListView")
+local MMRichText = requireM(g_require_com .. "MMRichText")
 
 
 local LoginScene = class("LoginScene",function()
@@ -75,26 +76,41 @@ function LoginScene:createLayerLabelAndBtn()
 		fontSize = 24,target = self,txt = "aaaaaaaaaaaaaaaaaaaaaaaaaaaa"}
 	local tempField = g_GUIcreator:createTextField(configField)	
 	
-	local cellSize = cc.size(250,150)
-	local tempListView = MMListView.new()
-	layer:addChild(tempListView)
-	tempListView:setSize(cc.size(500,500))
-	tempListView:setCellSize(cellSize)
-	tempListView:setRowNum(2)
-	tempListView:setPosition(ccp(300,100))
-	tempListView:setBackGroundImage()
-	tempListView:createCellFunc(function (index,data,pos)
-		print("see the cell data>>" .. data)
-		local node = cc.Node:create()
-		local configBtn = {parent = node,pos = cc.p(cellSize.width/2, cellSize.height/2),
-			normalUrl = g_load_ui .. "PlayButton.png", txt = "开始", txtColor = cc.c3b(255,0,0),
-			clickEvent = self.callFun, target = self}
-		local box = g_GUIcreator:createButton(configBtn)
-		return node	
-	end)
+	 local cellSize = cc.size(250,150)
+	-- local tempListView = MMListView.new()
+	-- layer:addChild(tempListView)
+	-- tempListView:setSize(cc.size(500,500))
+	-- tempListView:setCellSize(cellSize)
+	-- tempListView:setRowNum(2)
+	-- tempListView:setPosition(ccp(300,100))
+	-- tempListView:setBackGroundImage()
+	-- tempListView:createCellFunc(function (index,data,pos)
+		-- print("see the cell data>>" .. data)
+		-- local node = cc.Node:create()
+		-- local configBtn = {parent = node,pos = cc.p(cellSize.width/2, cellSize.height/2),
+			-- normalUrl = g_load_ui .. "PlayButton.png", txt = "开始", txtColor = cc.c3b(255,0,0),
+			-- clickEvent = self.callFun, target = self}
+		-- local box = g_GUIcreator:createButton(configBtn)
+		-- return node	
+	-- end)
 	
-	tempListView:reloadByData({1,2,3,4,5,6,7,8,9,10,11})
+	-- tempListView:reloadByData({1,2,3,4,5,6,7,8,9,10,11})
 	-- tempListView:showRect()
+	
+	local richText = MMRichText.new(cc.size(600,600))
+	richText:setPosition(cc.p(300,100))
+	layer:addChild(richText)
+	richText:addText({color = cc.c3b(255,0,255),text = "我与地坛",fontSize = 24})
+	richText:addImage({color = cc.c3b(255,0,255),filePath = g_load_ui .. "Star.png"})
+	
+	
+	local node = cc.Node:create()
+	local configBtn = {parent = node,pos = cc.p(cellSize.width/2, 0),
+		normalUrl = g_load_ui .. "PlayButton.png", txt = "开始", txtColor = cc.c3b(255,0,0),
+		clickEvent = self.callFun, target = self}
+	local box = g_GUIcreator:createButton(configBtn)
+	richText:addCustomNode({color = cc.c3b(255,0,255),customNode = node})
+	
 	
 	
 	
