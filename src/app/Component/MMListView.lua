8b3dcdb8917937bcccfc4715363dbf9914d97c18
abelHan <talance@126.com>
2015-08-21 11:54:37 +0800

    abel
    完善ListView，添加了背景和区域显示

--- src/app/Component/MMListView.lua
+++ src/app/Component/MMListView.lua
@@ -27,16 +27,41 @@ MMListView
 -- tempListView:reloadByData(data)
 		
 local MMListView = class("MMListView",function()
-	return ccui.ScrollView:create()
+	return cc.Node:create() 
 end)
 
 function MMListView:ctor()
+
+	local scrollView = ccui.ScrollView:create()
+	self:addChild(scrollView,2)
+	self.scrollView = scrollView
+
 	self.rowNum = 1 						--列数
 	self.cellTab = {} 						--cell对象
 	self.cellCreateCallBack = nil 			--创建cell时用到的回调函数
 	self.cellSize = nil 					--cell大小
 	self.cellPos  = {}						--cell所有位置信息
 	self.data  =nil   						--创建cell时用到的数据
+end
+
+function MMListView:setBackGroundImage(pngUrl)
+
+	local showBg = ccui.ImageView:create()
+	self.showBg = showBg
+	
+	showBg:setScale9Enabled(true)
+	if self.contentSize then
+		showBg:setContentSize(self.contentSize)
+		self:adjustBg(self.contentSize)		
+	end
+	showBg:loadTexture(pngUrl or g_load_ui .."defaul/scrollview_bg.png")
+	self:addChild(showBg,1)
+
+	
+end
+
+function MMListView:initData()
+
 end
 
 function MMListView:setRowNum(row)
@@ -57,7 +82,7 @@ function MMListView:calculateCellsPos()
 	local lineNum = math.ceil(cellNum/self.rowNum) - 1
 	local cellSize = self:getCellSize()
 	local tempX,tempY = 0,cellSize.height *lineNum - 10
-	local visibleSize = self:getContentSize()
+	local visibleSize = self.contentSize
 	
 	for i = 1,cellNum do
 		self.cellPos[i] = cc.p(tempX, tempY)
@@ -73,9 +98,9 @@ function MMListView:calculateCellsPos()
 	
 	local tempHeihgt = cellSize.height * (lineNum + 1)
 	if tempHeihgt >= visibleSize.height then
-		self:setBounceEnabled(true)
+		self.scrollView:setBounceEnabled(true)
 	else
-		self:setBounceEnabled(false)
+		self.scrollView:setBounceEnabled(false)
 		--不够时集体上移
 		local length = visibleSize.height - tempHeihgt
 		for i,v in ipairs(self.cellPos) do
@@ -84,7 +109,7 @@ function MMListView:calculateCellsPos()
 	end
 	
 	--设置参数
-	
+	self.scrollView:setInnerContainerSize(cc.size(cellSize.width * self.rowNum,cellSize.height * (lineNum + 1)))
 	
 end
 
@@ -108,10 +133,29 @@ function MMListView:getCell()
 	return self.cellTab
 end
 
+function MMListView:setSize(contentSize)
+	self.contentSize = contentSize
+	if self.scrollView then 
+		self.scrollView:setContentSize(contentSize) 
+	end
+	
+	self:adjustBg(contentSize)
+
+end
+
+function MMListView:adjustBg(contentSize)
+	if contentSize == nil or self.showBg == nil then return  end
+	if self.showBg then 
+		local bgSize = cc.size(contentSize.width +20,contentSize.height + 20)
+		self.showBg:setContentSize(bgSize)
+		self.showBg:setPosition(cc.p(contentSize.width/2,contentSize.height/2))
+	end	
+end
+
 --isFramingLoad 是否分帧加载
 function MMListView:reloadByData(data, isFramingLoad)
 	self.cellTab = {}
-	self:removeAllChildren()
+	self.scrollView:removeAllChildren()
 	self.cellData = data
 	if #data <= 0 then
 		return
@@ -128,7 +172,7 @@ function MMListView:reloadByData(data, isFramingLoad)
 		if cellView then
 			cellView:setPosition(self.cellPos[i])
 			self.cellTab[#self.cellTab + 1] = cellView
-			self:addChild(cellView)		
+			self.scrollView:addChild(cellView)		
 		end
 		
 	end
@@ -159,11 +203,13 @@ end
 
 --显示整个控件区域
 function MMListView:showRect()
-
+	local tempSize = self.contentSize
+	if tempSize == nil then return end
+	
+	local draw = cc.DrawNode:create()
+	self:addChild(draw,3)
+	draw:drawRect(cc.p(0,0),cc.p(tempSize.width,tempSize.height),cc.c4f(1,1,0,1))
 end
-
-
-
 
 
 return MMListView
--- src/app/view/LoginScene.lua
+++ src/app/view/LoginScene.lua
@@ -3,6 +3,10 @@
 
 --]]
 
+
+local MMListView = requireM(g_require_com .. "MMListView")
+
+
 local LoginScene = class("LoginScene",function()
 	return cc.Scene:create()
 end)
@@ -53,7 +57,7 @@ function LoginScene:createLayerLabelAndBtn()
 	local tempBtn = g_GUIcreator:createButton(configBtn)
 	
 	--label
-	local configLabel = {parent = layer,pos = cc.p(200, 500),
+	local configLabel = {parent = layer,pos = cc.p(100, 500),
 		txt = "人生若是久长时,\n又岂在朝朝暮暮",fontSize = 28,txtColor = cc.c3b(255,0,0),areaSize = cc.size(300,0)}
 	local tempLabel = g_GUIcreator:createLabel(configLabel)
 	self.label = tempLabel
@@ -63,13 +67,37 @@ function LoginScene:createLayerLabelAndBtn()
 		pngUrl = g_load_ui .. "Star.png"}
 	local tempImg = g_GUIcreator:createImg(configImg)
 	
-	local configCheck ={parent = layer,pos = cc.p(200,200),
+	local configCheck ={parent = layer,pos = cc.p(100,200),
 		callBack = self.callFun1,target = self,txt = "字体颜色"}
 	local tempCheck = g_GUIcreator:createMMCheck(configCheck)
 
-	local configField ={parent = layer,pos = cc.p(200,400),areaSize=cc.size(200,50),
+	local configField ={parent = layer,pos = cc.p(100,400),areaSize=cc.size(200,50),
 		fontSize = 24,target = self,txt = "aaaaaaaaaaaaaaaaaaaaaaaaaaaa"}
 	local tempField = g_GUIcreator:createTextField(configField)	
+	
+	local cellSize = cc.size(250,150)
+	local tempListView = MMListView.new()
+	layer:addChild(tempListView)
+	tempListView:setSize(cc.size(500,500))
+	tempListView:setCellSize(cellSize)
+	tempListView:setRowNum(2)
+	tempListView:setPosition(ccp(300,100))
+	tempListView:setBackGroundImage()
+	tempListView:createCellFunc(function (index,data,pos)
+		print("see the cell data>>" .. data)
+		local node = cc.Node:create()
+		local configBtn = {parent = node,pos = cc.p(cellSize.width/2, cellSize.height/2),
+			normalUrl = g_load_ui .. "PlayButton.png", txt = "开始", txtColor = cc.c3b(255,0,0),
+			clickEvent = self.callFun, target = self}
+		local box = g_GUIcreator:createButton(configBtn)
+		return node	
+	end)
+	
+	tempListView:reloadByData({1,2,3,4,5,6,7,8,9,10,11})
+	-- tempListView:showRect()
+	
+	
+	
 	return layer
 end
 
